a1ad87fbe514681ca1544f012075680471a950bd
Francois Ribalet <ribalet@uw.edu>
2018-11-15 15:23:02 -0800

    added MGL1704 cruise validation

--- 2.cruise_calibration/1.EVT_filtration_screening.R
+++ 2.cruise_calibration/1.EVT_filtration_screening.R
@@ -1,8 +1,9 @@
 library(popcycle)
 library(dplyr)
-cols <- colorRampPalette(c("blue4", "royalblue4", "deepskyblue3", "seagreen3", "yellow", "orangered2", "darkred"))
+library(splancs)
+library(scales)
 
-#########################################
+  #######################################
 ## 0. DOWNLOAD THE RAW DATA using DAT ###
 #########################################
 # dat://fa2bb5981465ff583e091071f2f2c5e5f1b118c5219971a2dde107e4dec25e63
@@ -23,25 +24,25 @@ beads.coord <- read.csv("https://raw.githubusercontent.com/armbrustlab/seaflow-f
 setwd("~/Documents/DATA/Codes/seaflow-virtualcore/2.cruise_calibration/")
 
 
-gate <- FALSE
-allcruises <- c("SCOPE_6", "SCOPE_2", "DeepDOM", "MBARI_1","SCOPE_16","Thompson_9")
+gate <- F
+allcruises <- c("SCOPE_6", "SCOPE_2", "DeepDOM", "MBARI_1","SCOPE_16","Thompson_9", "MGL1704")
 
 
 for(cruise in allcruises){
 
-  #cruise <- allcruises[2]
+  #cruise <- allcruises[7]
 
   print(cruise)
   ### Get EVT list
   if(cruise == "SCOPE_6" | cruise == "SCOPE_16" | cruise == "SCOPE_2") list <- list.files(paste0(path.to.data,cruise,"data"), "00-00$",full.names=T, recursive=T)
   if(cruise == "DeepDOM"| cruise == "MBARI_1" | cruise == "Thompson_9") list <- list.files(paste0(path.to.data,cruise,"data"), ".evt$",full.names=T, recursive=T)
   if(cruise == "Thompson_9") list <- list.files(paste0(path.to.data,cruise,"data"), ".evt",full.names=T, recursive=T)
-  if(cruise == "SCOPE_2") list <- list.files(paste0(path.to.data,cruise,"data"), "00-00.gz",full.names=T, recursive=T)
+  if(cruise == "SCOPE_2" | cruise == "MGL1704") list <- list.files(paste0(path.to.data,cruise,"data"), "00-00.gz",full.names=T, recursive=T)
 
 
   if(cruise == "SCOPE_6" | cruise == "Thompson_9" | cruise == "SCOPE_2") inst <- 740
   if(cruise == "DeepDOM" | cruise == "MBARI_1") inst <- 989
-  if(cruise == "SCOPE_16") inst <- 751
+  if(cruise == "SCOPE_16"| cruise == "MGL1704") inst <- 751
 
   # SLOPES
   slopes <- read.csv("seaflow_filter_slopes.csv")
@@ -80,10 +81,10 @@ for (file in list){
     # if(nrow(evt.)> 100000){df <- sample_n(evt., 100000)
     #   } else df <- evt.
     # par(mfrow=c(2,2))
-    # plot.cytogram(df, "D1", "D2"); abline(b=notch, a=origin)
-    # plot.cytogram(aligned., "D1", "D2"); abline(b=1, a=origin)
-    # plot.cytogram(df, "fsc_small", "D1"); abline(b=notch.small.D1, a=offset.small.D1,col=2); abline(b=notch.large.D1, a=offset.large.D1,col=3); points(beads.fsc, beads.d1,pch=16, col=3)
-    # plot.cytogram(df, "fsc_small", "pe"); abline(b=notch.small.D2, a=offset.small.D2,col=2); abline(b=notch.large.D2, a=offset.large.D2,col=3); points(beads.fsc, beads.d2,pch=16, col=3)
+    # plot_cyt(df, "D1", "D2"); abline(b=notch, a=origin)
+    # plot_cyt(aligned., "D1", "D2"); abline(b=1, a=origin)
+    # plot_cyt(df, "fsc_small", "D1"); abline(b=notch.small.D1, a=offset.small.D1,col=2); abline(b=notch.large.D1, a=offset.large.D1,col=3); points(beads.fsc, beads.d1,pch=16, col=3)
+    # plot_cyt(df, "fsc_small", "pe"); abline(b=notch.small.D2, a=offset.small.D2,col=2); abline(b=notch.large.D2, a=offset.large.D2,col=3); points(beads.fsc, beads.d2,pch=16, col=3)
 
     screening <- c(0,2500,5000,7500,10000, 1, 2, 3, 4)
 
@@ -139,7 +140,7 @@ for (file in list){
       opp$pop <- 0
       opp. <- subset(opp, fsc_small > 2000)
 
-        if(gate) {plot.cytogram(opp , "fsc_small", "pe")
+        if(gate) {plot_cyt(opp , "fsc_small", "pe")
                         poly.beads <- getpoly(quiet=TRUE)
                         write.csv(poly.beads,paste0(cruise,"data/", basename(file),"-polybeads.csv"),quote=F, row.names=F)
                       }
@@ -155,7 +156,7 @@ for (file in list){
         opp[row.names(s),'pop'] <- "synecho"
 
       x <- subset(opp, pop==0)
-        if(gate){plot.cytogram(opp , "fsc_small", "chl_small")
+        if(gate){plot_cyt(opp , "fsc_small", "chl_small")
                         poly.pro <- getpoly(quiet=TRUE)
                         write.csv(poly.pro,paste0(cruise,"data/", basename(file),"-polypro.csv"),quote=F, row.names=F)
                       }
@@ -179,8 +180,8 @@ for (file in list){
           if(nrow(opp)> 100000){opp. <- sample_n(opp, 100000)
              } else opp. <- opp
           par(mfrow=c(2,2))
-          plot.cytogram(aligned. ,  "fsc_small", "D1"); abline(b=notch.small.D1, a=offset.small.D1,col=2); abline(b=notch.large.D1, a=offset.large.D1,col=3); points(beads.fsc, beads.d1,pch=16, col=3)
-          plot.cytogram(aligned. , "fsc_small", "D2"); abline(b=notch.small.D2, a=offset.small.D2,col=2); abline(b=notch.large.D2, a=offset.large.D2,col=3); points(beads.fsc, beads.d2,pch=16, col=3)
+          plot_cyt(aligned. ,  "fsc_small", "D1"); abline(b=notch.small.D1, a=offset.small.D1,col=2); abline(b=notch.large.D1, a=offset.large.D1,col=3); points(beads.fsc, beads.d1,pch=16, col=3)
+          plot_cyt(aligned. , "fsc_small", "D2"); abline(b=notch.small.D2, a=offset.small.D2,col=2); abline(b=notch.large.D2, a=offset.large.D2,col=3); points(beads.fsc, beads.d2,pch=16, col=3)
           plot(opp.[, c("fsc_small", "pe")], cex = 1, pch = 16, col = alpha(as.numeric(as.factor(opp.$pop)), 0.4))
           plot(opp.[, c("fsc_small", "chl_small")], cex = 1, pch = 16, col = alpha(as.numeric(as.factor(opp.$pop)), 0.4))
           dev.off()
@@ -232,8 +233,8 @@ library(popcycle)
 
 setwd("~/Documents/DATA/Codes/seaflow-virtualcore/2.cruise_calibration/")
 
-allcruises <- c("SCOPE_6", "SCOPE_2" , "DeepDOM", "MBARI_1","SCOPE_16","Thompson_9")
-cruise <- allcruises[2]
+allcruises <- c("SCOPE_6", "SCOPE_2", "DeepDOM", "MBARI_1","SCOPE_16","Thompson_9", "MGL1704")
+cruise <- allcruises[7]
 
 DF <- NULL
 for(cruise in allcruises){
@@ -290,12 +291,23 @@ if(cruise == 'SCOPE_2'){influx <- read.csv(paste0(cruise,"data/abundance_table.c
                           influx$pico <- influx[,"pico_infl"]
                         }
 
+if(cruise == 'MGL1704'){influx <- read.csv(paste0(cruise,"data/all_station_curated.csv"))
+                          influx$time <- as.POSIXct(influx$time_GMT, format="%m/%d/%y %H:%M", tz="GMT")
+                          influx <- subset(influx, depth_m < 20)
+                          pro <- subset(influx, population == 'prochloro')
+                          syn <- subset(influx, population == 'synecho')
+                          picoeuk <- subset(influx, population == 'picoeuk')
+                          influx <- data.frame(cbind(time=pro$time, pro=pro$abundance, syn=syn$abundance, pico=picoeuk$abundance))
+                          influx$time <- as.POSIXct(influx$time, origin='1970-01-01',tz="GMT")
+                                        }
+
+
 influx <- influx[order(influx$time), ]
 
 ### SFL
 if(cruise == "SCOPE_6" | cruise == "Thompson_9" | cruise == "SCOPE_2") inst <- 740
 if(cruise == "DeepDOM" | cruise == "MBARI_1") inst <- 989
-if(cruise == "SCOPE_16") inst <- 751
+if(cruise == "SCOPE_16"| cruise == "MGL1704") inst <- 751
 
 if(cruise != 'MBARI_1'){
       sfl <- read.delim(paste0("https://raw.githubusercontent.com/armbrustlab/seaflow-sfl/master/curated/", cruise,"_",inst, ".sfl"))
@@ -306,7 +318,7 @@ if(cruise != 'MBARI_1'){
 ALL <- read.csv(paste0(cruise,"data/seaflow-summary.csv"))
 if(cruise == "SCOPE_16") ALL <- ALL[!(ALL$file =="2016-04-26T15-07-38-00-00"),]
 if(cruise == "DeepDOM") ALL <- ALL[!(ALL$file =="2013_094/321.evt" | ALL$file=="2013_124/409.evt"),]
-if(cruise == "Thompson_9" | cruise == "SCOPE_2") ALL$file <- sub(".gz","", ALL$file)
+if(cruise == "Thompson_9" | cruise == "SCOPE_2" | cruise == 'MGL1704') ALL$file <- sub(".gz","", ALL$file)
 
   ALL$cruise <- cruise
   id2 <- match(ALL[,'file'],as.character(sfl[,"FILE"]))
@@ -508,28 +520,30 @@ df2 <- aggregate(df1, by=list(df1$time), FUN=function(x) mean(x, na.rm=T))
 df3 <- aggregate(df1, by=list(df1$time), FUN=function(x) sd(x, na.rm=T))
   df3$cruise <- df2b$cruise
 
-data <- tibble(cruise=as.factor(rep(as.character(df2$cruise), 3)),
-                  time=as.POSIXct(rep(df2$time,3)),
-                  population=rep(c("prochloro","synecho","picoeuk"), each=nrow(df2)),
-                  ref=c(df2[,paste0("pro.influx")],df2[,paste0("syn.influx")], df2[,paste0("pico.influx")]),
-                  abundance=c(df2[,paste0("pro.seaflow.each",s)],df2[,paste0("syn.seaflow.each",s)], df2[,paste0("pico.seaflow.median",s)]))
+data <- tibble(cruise=as.factor(rep(as.character(df2$cruise), 6)),
+                  time=as.POSIXct(rep(df2$time,6)),
+                  population=rep(c("prochloro","synecho","picoeuk"), each=nrow(df2)*2),
+                  instrument=rep(c("SeaFlow","Influx"),nrow(df2)*3),
+                  abundance=c(df2[,paste0("pro.seaflow.each",s)],df2[,paste0("pro.influx")],
+                              df2[,paste0("syn.seaflow.each",s)], df2[,paste0("syn.influx")],
+                              df2[,paste0("pico.seaflow.median",s)], df2[,paste0("pico.influx")])
+                              )
+
 #remove SeaFlow pico data for Thompson_9
 data[which(data$population == 'picoeuk' & data$cruise == 'Thompson_9'), 'abundance'] <- NA
 
 group.colors <- c(unknown='grey', beads='red3', prochloro=viridis(4)[1],synecho=viridis(4)[2],picoeuk=viridis(4)[3], croco=viridis(4)[4])
 
 
-levels(data$cruise) <- c("KN210-04", "CN11","KOK1606","KM1502","KM1513","TN271")
+levels(data$cruise) <- c("KN210-04", "CN11","MGL1704" ,"KOK1606","KM1502","KM1513","TN271")
 
 
 p <- data %>%
       ggplot() +
-      geom_point(aes(x=time, y=abundance,fill=population), pch=21, alpha=0.5, size=6) +
-      geom_point(aes(x=time, y=ref,fill=population), pch=21, size=2, show.legend=F) +
+      geom_point(aes(x=time, y=abundance,fill=population, size=instrument), pch=21, alpha=0.5, show.legend=T) +
       facet_wrap( ~ cruise, scales='free_x') +
       scale_y_continuous(trans= 'log10') +
-      scale_color_manual(values=group.colors) +
-      scale_fill_manual(values=group.colors, name="SeaFlow") +
+      scale_fill_manual(values=group.colors) +
       labs(x="", y="Abundance (cells µL-1)") +
       theme_bw()
 p
